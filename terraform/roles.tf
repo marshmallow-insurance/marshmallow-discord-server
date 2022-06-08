0bc7cee048fa8a4ad03d266b41368d90e6179760
Charlie Collard <charlie.collard@marshmallow.co>
2022-06-08 15:38:19 +0100

    Remove everything except roles and module (#1)
    * Remove everything except roles and module
    
    * fmt

---
+++ terraform/roles.tf
@@ -0,0 +1,33 @@
+resource "discord_role" "moderator" {
+  server_id = discord_server.server.id
+  name      = "Moderator"
+  position  = 0
+  color     = data.discord_color.terraform_purple.dec
+}
+
+resource "discord_role" "basic_user_role" {
+  server_id   = discord_server.server.id
+  name        = "Basic User"
+  permissions = data.discord_permission.basic_user.allow_bits
+  color       = data.discord_color.marshmallow_pink.dec
+  hoist       = true
+  mentionable = true
+  position    = 1
+}
+
+data "discord_permission" "basic_user" {
+  view_channel        = "allow"
+  send_messages       = "allow"
+  use_external_emojis = "allow"
+  use_vad             = "deny"
+  priority_speaker    = "deny"
+}
+
+data "discord_color" "marshmallow_pink" {
+  hex = "#ff4e75"
+}
+
+data "discord_color" "terraform_purple" {
+  hex = "#8040c9"
+}
+
--- terraform/server.tf
+++ terraform/server.tf
@@ -1,5 +1,4 @@
 resource "discord_server" "server" {
-  name                    = "Marshmallow!"
-  region                  = "us-south"
-  explicit_content_filter = 1
+  name   = "Marshmallow!"
+  region = "us-south"
 }
