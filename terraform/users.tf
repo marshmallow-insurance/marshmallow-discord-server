8af0d33c6f58a4834e0c3ea16025339edd39711b
Charlie Collard <charlie.collard@marshmallow.co>
2022-06-09 20:32:16 +0100

    Fix name

--- terraform/users.tf
+++ terraform/users.tf
@@ -41,7 +41,7 @@ module "add_Thelma" {
 module "add_danny" {
   source             = "./modules/new-member"
   server_id          = discord_server.server.id
-  username           = "kuwotu"
+  username           = "Executioner"
   user_discriminator = "7363"
   display_name       = "Daniel"
   welcome_channel_id = discord_text_channel.welcome.id
