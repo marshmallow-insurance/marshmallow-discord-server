f0718f396c04050d62628193aa617cad3944f99c
Daniel Ayesoro <dayesoro@gmail.com>
2022-06-09 20:26:58 +0100

    Add danny (#9)
    * Add danny
    
    * Add danny
    
    * Add danny
    
    Co-authored-by: Charlie Collard <charlie.collard@marshmallow.co>

--- terraform/users.tf
+++ terraform/users.tf
@@ -37,3 +37,13 @@ module "add_Thelma" {
   welcome_channel_id = discord_text_channel.welcome.id
   user_role_id       = discord_role.basic_user_role.id
 }
+
+module "add_danny" {
+  source             = "./modules/new-member"
+  server_id          = discord_server.server.id
+  username           = "Danny"
+  user_discriminator = "7363"
+  display_name       = "Daniel"
+  welcome_channel_id = discord_text_channel.welcome.id
+  user_role_id       = discord_role.basic_user_role.id
+}
